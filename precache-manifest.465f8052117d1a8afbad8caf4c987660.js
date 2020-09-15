38305f9bca1df0b66a2b78869daecffe00b6f465
Gaga Liu <gaga.liu@framy.co>
2020-09-15 17:19:20 +0800

    Updates

---
+++ precache-manifest.465f8052117d1a8afbad8caf4c987660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "247179cd064b79191d3a8ab1e4c2094d",
+    "url": "/Turntable/index.html"
+  },
+  {
+    "revision": "2bab6068927031075d6c",
+    "url": "/Turntable/static/js/2.d471ba27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Turntable/static/js/2.d471ba27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "325a04e9457eda70b35a",
+    "url": "/Turntable/static/js/main.c85b36e0.chunk.js"
+  },
+  {
+    "revision": "27edd1edf65c5383a537",
+    "url": "/Turntable/static/js/runtime-main.ecf22548.js"
+  }
+]);
